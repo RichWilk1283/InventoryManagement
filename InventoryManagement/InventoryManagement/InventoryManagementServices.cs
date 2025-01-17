e16b31197f0df7c50076efb8a1198667ee10af37
Richard Wilkinson <richardwilkinson1283@gmail.com>
2025-01-17 10:28:48 +0000

    Additional code Added simple class for InventoryManagerService, and a initial menu system.

---
+++ InventoryManagement/InventoryManagement/InventoryManagementServices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+  public class InventoryManagementServices
+  {
+    public string Name { get; set; }
+    public Dictionary<string, int> InventoryList { get; set; }
+
+    public InventoryManagementServices(string name)
+    {
+      InventoryList = new Dictionary<string, int>();
+      Name = name;        
+    }
+
+    public void getInventory()
+    {
+      Console.WriteLine($"{Name}, has the following intems:");
+      foreach (var item in InventoryList)
+      {
+        
+        Console.WriteLine(item);          
+      }
+    }
+
+    public void addInventory()
+    {
+
+    }
+  }
+
+}
--- InventoryManagement/InventoryManagement/Program.cs
+++ InventoryManagement/InventoryManagement/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using InventoryManagement;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.WriteLine("Welcome to inventory management app.");
+    Console.WriteLine("Please name your company");
+
+    string name = Console.ReadLine();
+
+    var newInventoryManagement = new InventoryManagementServices(name);
+  }
+}
